3c88e48530577a51f0736c4ab50efef361ee7800
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2024-01-19 17:32:57 +0600

    features added

--- app.js
+++ app.js
@@ -4,9 +4,30 @@ const loadData = () => {
         .then(data => showData(data.data.tools))
 }
 const showData = (data) => {
-    console.log(data)
-    const { description, features, id, links, name, published_in } = data;
+    // console.log(data)
+    const cardsContainer = document.getElementById('cards-container');
+    cardsContainer.innerHTML = "";
+    data = data.slice(0, 6)
     data.forEach(singleData => {
+        const { description, features, id, links, name, published_in, image } = singleData;
         console.log(singleData)
+        cardsContainer.innerHTML += `
+        <div class="col">
+            <div class="card h-100 p-3">
+                <img src="${image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="">Features</h5>
+                    <ol>
+                        <li>${features[0]}</li>
+                        <li>${features[1]}</li>
+                        <li>${features[2]}</li>
+                    </ol>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        `
     })
 }
